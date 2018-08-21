65224412dbb78eabc3cbd39f512fb8678b26a24e
xieqiang <xieqiang@wdai.com>
2018-08-21 15:17:37 +0800

    FeignClient的使用

--- src/main/java/com/example/demo/controller/ComputeController.java
+++ src/main/java/com/example/demo/controller/ComputeController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import org.apache.log4j.Logger;
-import org.bouncycastle.asn1.ocsp.ResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -11,6 +10,10 @@ import org.springframework.web.bind.annotation.*;
 /**
  * Created by WD42700 on 2018/8/19.
  */
+
+//@RestController注解相当于@ResponseBody ＋ @Controller   RequestMapping需要再单独定义
+//@RestController虽然继承了@Controller，但是却不能返回jsp，即试图解析器InternalResourceViewResolver会失效。value属性不要使用
+@RequestMapping("/compute")
 @RestController
 public class ComputeController {
 
@@ -37,6 +40,18 @@ public class ComputeController {
     public void sub(@PathVariable("a")Long a) {
         String  s=  "大家好，我是减法 参数为："+(a-100);
         System.out.println(s);
+    }
+
+    /**
+     * 乘法
+     * @param
+     * @return
+     */
+    @GetMapping("/multi/{a}/{b}")
+    public Long multi(@PathVariable("a")Long a,@PathVariable("b")Long b) {
+        String  s=  "大家好，我是乘法 参数为   a:"+a+"   b:"+b;
+        System.out.println(s);
+        return  a*b;
     }
 
 
