14924e3d17bdfbca9f867420905c496c7bd5bc9c
xieqiang <xieqiang@wdai.com>
2018-12-04 21:43:54 +0800

    返回值类型改成String

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class Application {
 
+	//springcloud暴露的服务接口 必须和Application在一个包底下、或者在他的子包底下
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
